05fc0205d34c365a8a07ac534626c541fc62d8e8
Keivon <keivonproctor@hotmail.com>
2020-06-29 22:26:47 +0100

    Add microphone support

--- main.py
+++ main.py
@@ -12,5 +12,24 @@ def speak(text):
     tts.save(filename)
     playsound.playsound(filename)
 
-speak("Hey, look at me")
+def get_audio():
+	r = sr.Recognizer()
+	with sr.Microphone() as source:
+		audio = r.listen(source)
+		said = ""
+
+		try:
+		    said = r.recognize_google(audio)
+		    print(said)
+		except Exception as e:
+		    print("Exception: " + str(e))
+
+	return said
+
+text = get_audio()
+
+if "hello" in text:
+    speak("hello, how are you?")
+elif "what is your name" in text:
+    speak("My name is Angie")
 
